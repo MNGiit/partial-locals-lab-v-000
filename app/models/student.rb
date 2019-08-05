99bfaea82e2cafca351d6d8e1973d877cf0ad5c9
Mohammed Noori <kmanoorib@gmail.com>
2019-08-05 01:28:04 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,13 +13,15 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
-  
+  validates :name, presence: true
+  validates :hometown, presence: true
+  validates :birthday, presence: true
+
   def self.search(query)
-    if student == ''
-      self.all
+    if query.present?
+      where('NAME like ?', "%#{query}%")
     else
-      # arr.select {|s| s.include? 'sa'}
-      self.all.select { |s| s.name.downcase.include?(student) }
+      self.all
     end
   end
 end
