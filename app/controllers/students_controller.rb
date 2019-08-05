af22b859e61e5d76dea64df47eb79377c9b2c772
Mohammed Noori <kmanoorib@gmail.com>
2019-08-05 01:24:40 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -22,7 +22,7 @@ class StudentsController < ApplicationController
 
   def index
     # @students = Student.all #use custom search
-    @students = Student.search(params[:findme])
+    @students = Student.search(params[:query])
     
   end
 
